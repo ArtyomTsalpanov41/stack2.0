1ef0100a4f0bb6de80ae213387634b0fd2fd2dfa
ArtyomTsalpanov <Tsalpan0v@mail.ru>
2019-06-06 02:54:42 +0300

    Stack

---
+++ cmake-build-debug/CMakeFiles/ZhirTema.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZhirTema.dir/main.cpp.obj"
+  "ZhirTema.pdb"
+  "ZhirTema.exe"
+  "ZhirTema.exe.manifest"
+  "ZhirTema.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZhirTema.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
